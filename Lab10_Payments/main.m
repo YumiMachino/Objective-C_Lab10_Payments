7ebfde4eec36843e6872a8cf28dd2d9883e81ed7
YumiMachino <y.y.1515m@gmail.com>
2021-03-03 10:19:42 -0800

    Initial Commit

---
+++ Lab10_Payments/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab10_Payments
+//
+//  Created by Yumi Machino on 2021/03/03.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
